0282f82872a1851a3ce5642e947e2c162623daca
poojashree-easwaramoorthy <poojashreeeaswaramoorthy@gmail.com>
2025-01-01 23:43:33 +0530

    Day 5 tasks

--- kecComputing/src/App.jsx
+++ kecComputing/src/App.jsx
@@ -4,6 +4,9 @@ import Navbar from "./Components/FunctionalComponents/Navbar";
 import Home from "./Components/FunctionalComponents/Home";
 import About from "./Components/FunctionalComponents/About";
 import Gallery from "./Components/FunctionalComponents/Gallery";
+import UseEffect from "./Components/FunctionalComponents/UseEffect";
+import UseContext from "./Components/FunctionalComponents/UseContext";
+import UseRef from "./Components/FunctionalComponents/UseRef";
 import Contact from "./Components/FunctionalComponents/Contact";
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
@@ -16,6 +19,9 @@ function App(){
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About College="Kongu Engineering" clg1="Kongu Arts" clg2="Naturopathy"/>}/>
           <Route path="/gallery" element={<Gallery/>}/>
+          <Route path="/Useeffect" element={<UseEffect/>}/>
+          <Route path="/useref" element={<UseRef/>}/>
+          <Route path="/usecontext" element={<UseContext/>}/>
           <Route path="/contact" element={<Contact/>}/>
         </Routes>
         </BrowserRouter>
--- kecComputing/src/Components/FunctionalComponents/Navbar.jsx
+++ kecComputing/src/Components/FunctionalComponents/Navbar.jsx
@@ -9,7 +9,11 @@ const Navbar = () => {
                     <li><Link to='/'>Home</Link></li>
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/gallery">Gallery</Link></li>
+                    <li><Link to="/useeffect">UseEffect</Link></li>
+                    <li><Link to="/usecontext">UseContext</Link></li>
+                    <li><Link to="/useref">UseRef</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
+                    
                 </ul>
             </nav>
         </header>
